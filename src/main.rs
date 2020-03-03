bb60076753939d3e903eb1ccc0cf8f8b56b2b3a9
Wei Liu <liuw@liuw.name>
2020-03-03 13:59:31 +0000

    Hit sphere shows normal

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,28 @@ use vec3::*;
 use ray::*;
 
 fn color(r: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Vec3::new(1.0, 0.0, 0.0)
+    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = Vec3::unit_vector(r.point_at_parameter(t) - Vec3::new(0.0,0.0,-1.0));
+        return 0.5 * Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
     }
     let unit_direction = Vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> f32 {
     let oc = r.origin() - *center;
     let a = Vec3::dot(&r.direction(), &r.direction());
     let b = 2.0 * Vec3::dot(&oc, &r.direction());
     let c = Vec3::dot(&oc, &oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+
+    (-b - discriminant.sqrt()) / (2.0 * a)
 }
 
 fn main() {
