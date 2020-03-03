e76f583031c4a4cdb961e2efc944fbd67c3d4f27
Wei Liu <liuw@liuw.name>
2020-03-03 11:26:26 +0000

    Use Ray in code

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ mod ray;
 use vec3::*;
 use ray::*;
 
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction = Vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -12,11 +18,17 @@ fn main() {
     println!("{} {}", nx, ny);
     println!("255");
 
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new(i as f32 / nx as f32,
-                j as f32 / ny as f32,
-                0.2);
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let r = Ray::new(&origin, &(lower_left_corner + u * horizontal + v * vertical));
+            let col = color(&r);
 
             let ir = (255.99 * col.r()) as i32;
             let ig = (255.99 * col.g()) as i32;
