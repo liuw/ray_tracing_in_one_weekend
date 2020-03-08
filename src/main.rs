41592f6ccf9e199464a40f073ff9ecdddbe9f7ee
Wei Liu <liuw@liuw.name>
2020-03-08 21:54:46 +0000

    Implement Metal material

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn random_in_unit_sphere() -> Vec3 {
     }
 }
 
-fn color(r: &Ray, world: &Vec<Box<&dyn Hittable>>) -> Vec3 {
+fn color(r: &Ray, world: &Vec<Box<&dyn Hittable>>, depth: u32) -> Vec3 {
     let mut rec = HitRecord {
         t: 0.0,
         p: Vec3::new(0.0, 0.0, 0.0),
@@ -39,8 +39,19 @@ fn color(r: &Ray, world: &Vec<Box<&dyn Hittable>>) -> Vec3 {
     };
 
     if hit(world, r, 0.001, std::f32::MAX, &mut rec) {
-        let target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * color(&Ray::new(&rec.p, &(target - rec.p)), &world);
+        let mut scattered = Ray::new(&Vec3::new(0.0, 0.0, 0.0), &Vec3::new(0.0, 0.0, 0.0));
+        let mut attenuation = Vec3::new(0.0, 0.0, 0.0);
+        if depth < 50
+            && rec
+                .material
+                .clone()
+                .unwrap()
+                .scatter(&r, &rec, &mut attenuation, &mut scattered)
+        {
+            return attenuation * color(&scattered, &world, depth + 1);
+        } else {
+            return Vec3::new(0.0, 0.0, 0.0);
+        }
     } else {
         let unit_direction = Vec3::unit_vector(r.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -48,9 +59,15 @@ fn color(r: &Ray, world: &Vec<Box<&dyn Hittable>>) -> Vec3 {
     }
 }
 
-#[derive(Copy, Debug, Clone)]
+#[derive(Clone)]
 pub struct Lambertian {
     albedo: Vec3,
+}
+
+impl Lambertian {
+    fn new(v: Vec3) -> Lambertian {
+        Lambertian { albedo: v }
+    }
 }
 
 impl Material for Lambertian {
@@ -69,6 +86,37 @@ impl Material for Lambertian {
     }
 }
 
+fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
+    *v - 2.0 * Vec3::dot(v, n) * *n
+}
+
+#[derive(Clone)]
+struct Metal {
+    albedo: Vec3,
+}
+
+impl Metal {
+    fn new(v: Vec3) -> Metal {
+        Metal { albedo: v }
+    }
+}
+
+impl Material for Metal {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Vec3,
+        scattered: &mut Ray,
+    ) -> bool {
+        let reflected = reflect(&Vec3::unit_vector(r_in.direction()), &rec.normal);
+        *scattered = Ray::new(&rec.p, &reflected);
+        *attenuation = self.albedo;
+
+        Vec3::dot(&scattered.direction(), &rec.normal) > 0.0
+    }
+}
+
 fn main() {
     let nx = 800;
     let ny = 400;
@@ -78,11 +126,32 @@ fn main() {
     println!("{} {}", nx, ny);
     println!("255");
 
-    let s1 = Sphere::new(&Vec3::new(0.0, 0.0, -1.0), 0.5, None);
-    let s2 = Sphere::new(&Vec3::new(0.0, -100.5, -1.0), 100.0, None);
+    let s1 = Sphere::new(
+        &Vec3::new(0.0, 0.0, -1.0),
+        0.5,
+        Some(Box::new(Lambertian::new(Vec3::new(0.8, 0.3, 0.3)))),
+    );
+    let s2 = Sphere::new(
+        &Vec3::new(0.0, -100.5, -1.0),
+        100.0,
+        Some(Box::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)))),
+    );
+    let s3 = Sphere::new(
+        &Vec3::new(1.0, 0.0, -1.0),
+        0.5,
+        Some(Box::new(Metal::new(Vec3::new(0.8, 0.6, 0.2)))),
+    );
+    let s4 = Sphere::new(
+        &Vec3::new(-1.0, 0.0, -1.0),
+        0.5,
+        Some(Box::new(Metal::new(Vec3::new(0.8, 0.8, 0.8)))),
+    );
+
     let world = vec![
         Box::new(&s1 as &dyn Hittable),
         Box::new(&s2 as &dyn Hittable),
+        Box::new(&s3 as &dyn Hittable),
+        Box::new(&s4 as &dyn Hittable),
     ];
 
     let cam = Camera::new();
@@ -95,7 +164,7 @@ fn main() {
                 let u = (i as f32 + random()) / nx as f32;
                 let v = (j as f32 + random()) / ny as f32;
                 let r = cam.get_ray(u, v);
-                col += color(&r, &world);
+                col += color(&r, &world, 0);
             }
             col /= ns as f32;
             // Gamma correction
